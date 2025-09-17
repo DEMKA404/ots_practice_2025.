c1f8a15887271597a306c393c427778bfa514a63
DANIL <ltheport@gmail.com>
2025-09-17 18:05:23 +0300

    task_1.py

---
+++ task_1.py
@@ -0,0 +1,18 @@
+# Лабораторная №1: Первичная инициализация
+# Курс: Основы теории систем
+# Студент: [Сычёв Даниил Владиславович]
+
+def get_system_info():
+
+    system_info = {
+        "student_name": "Сычёв Даниил Владиславович",
+        "academic_group": "ИВТИИбд-14",
+        "github_link": "https://github.com/DEMKA404"
+    }
+    return system_info
+
+if __name__ == "__main__":
+    info = get_system_info()
+    print("Информация о системе:")
+    for key, value in info.items():
+        print(f"- {key}: {value}")
